ba365c4d4be01fab7cd08808d9a3d1eb718538a9
Spoiler335 <sushantsai8@gmail.com>
2024-10-23 21:30:40 +0530

    Fix Damage Orb Functinality

--- Assets/Scripts/DamageOrb.cs
+++ Assets/Scripts/DamageOrb.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using UnityEngine;
 
 public class DamageOrb : MonoBehaviour
@@ -7,17 +8,25 @@ public class DamageOrb : MonoBehaviour
     private int damage = 10;
     private Rigidbody rb;
 
+    private static int count = 0;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Start()
+    {
+        Debug.Log($"Orb-{count} Instantiated");
+    }
+
     private void FixedUpdate()
     {
-        rb.MovePosition(transform.forward * speed * Time.deltaTime);
+        rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
     }
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log($"Orb-{count} Hit {other.name}");
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerController>().ApplyDamage(damage);
@@ -26,4 +35,6 @@ public class DamageOrb : MonoBehaviour
         Instantiate(hitVfx, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
+
+
 }
