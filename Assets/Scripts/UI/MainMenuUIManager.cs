e02bd7d8852d3fae7f7879d8d33b2cad4db47f3d
Spoiler335 <sushantsai8@gmail.com>
2024-10-30 19:30:28 +0530

    Setup the main Menu

---
+++ Assets/Scripts/UI/MainMenuUIManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        startButton.onClick.AddListener(OnStartButtonClicked);
+        quitButton.onClick.AddListener(OnQuitButtonClicked);
+    }
+
+    private void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene(SceneConstants.GAMEPLAY);
+    }
+
+    private void OnQuitButtonClicked() => Application.Quit();
+}
